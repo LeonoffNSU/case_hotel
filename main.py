fec8c209d07cf3b7892be92e413d308d42112078
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2024-04-11 23:31:47 +0700

    hh

---
+++ main.py
@@ -0,0 +1,3 @@
+type_room = {'one': 2900, 'two': 2300, 'middle_luxe': 3200, 'luxe': 4100}
+coefficient = {'standart': 1 , 'improve_standart': 1.2, 'apartament' : 1.5}
+food = {'without' : 0, 'breakfast' : 280, 'half_board': 1000}
