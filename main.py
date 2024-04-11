95081af8b212b8b76737fb894cafec8b9c89bcb1
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2024-04-12 02:05:09 +0700

    hh

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+import datetime
+day_1 = datetime.datetime(2020, 3, 1)
 type_room = {'one': 2900, 'two': 2300, 'middle_luxe': 3200, 'luxe': 4100}
-coefficient = {'standart': 1 , 'improve_standart': 1.2, 'apartament' : 1.5}
+coefficient = {'standart': 1, 'improve_standart': 1.2, 'apartment': 1.5}
 food = {'without' : 0, 'breakfast' : 280, 'half_board': 1000}
+
+
+with open('fund.txt', encoding= 'utf-8') as chrt:
+    fund_dict = {}
+    for line in chrt:
+        if 'одноместный' in line:
+            line = line.replace('одноместный', 'one')
+        if 'двухместный' in line:
+            line = line.replace('двухместный', 'two')
+        if 'полулюкс' in line:
+            line = line.replace('полулюкс', 'middle_luxe')
+        if 'люкс' in line:
+            line = line.replace('люкс', 'luxe')
+        if 'стандарт_улучшенный' in line:
+            line = line.replace('стандарт_улучшенный', 'improve_standart')
+        if 'стандарт' in line:
+            line = line.replace('стандарт', 'standart')
+        if 'апартамент' in line:
+            line = line.replace('апартамент', 'apartment')
+        line = line.split()
+        fund_dict[line[0]] = [line[1], int(line[2]), line[3]]
+    print(fund_dict)
+days = []
+for i in range(31):
+    days.append(str((day_1 + datetime.timedelta(i)).date()))
+print(days)
+
