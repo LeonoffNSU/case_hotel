a195e8117c9b20720d30e2256f35a33fc749a205
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2024-04-12 21:34:39 +0700

    hh

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def transformer(bad_date):
     return good_date
 
 
-day_1 = datetime.datetime(2020, 3, 1)
 type_room = {'one': 2900, 'two': 2300, 'middle_luxe': 3200, 'luxe': 4100}
 coefficient = {'standart': 1, 'improve_standart': 1.2, 'apartment': 1.5}
 food = {'without': 0, 'breakfast': 280, 'half_board': 1000}
