882b001bbadc1a98175f4855f3a13941abfe2baa
LeonoffNSU <k.leonov@g.nsu.ru>
2024-04-16 05:12:19 +0700

    разъебал

--- main.py
+++ main.py
@@ -6,30 +6,28 @@ def transformer(bad_date):
     return good_date
 
 
-def filter_cost(rooms: dict, requirement: np.str_):
-    requirement = int(requirement)
+def filter_cost(rooms: dict, opportunity: np.str_, arg_amount: int):
+    opportunity = int(opportunity)
     cost = {}
-    for room in rooms.keys():
-        if requirement >= fund_dict[room][3]:
-            cost[room] = rooms[room]
-    return cost
+    if arg_amount == int(clt[4]):
+        for room in rooms.keys():
+            if opportunity >= fund_dict[room][3]:
+                cost[room] = rooms[room]
+        return cost
+
+    else:
+        for room in rooms.keys():
+            if opportunity >= fund_dict[room][4]:
+                cost[room] = rooms[room]
+        return cost
 
 
-def free_room(bussy: dict):
-    free_rooms = {}
-    for room, free in bussy.items():
-        if free == 0:
-            free_rooms[room] = free
-    return free_rooms
-
-
-def suitable_quantity_filter(numbers: dict, requirement: int):
-    output_numbers = {}
-    for number in numbers:
-        if fund_dict[number][1] == requirement:
-            output_numbers[number] = numbers[number]
-
-    return output_numbers
+def suitable_quantity_filter(rooms: dict, requirement: int):
+    output_rooms = {}
+    for room in rooms:
+        if fund_dict[room][1] == requirement:
+            output_rooms[room] = rooms[room]
+    return output_rooms
 
 
 def future_busy(free_rooms: dict, entry_date: np.str_, amount_days: np.str_):
@@ -48,6 +46,62 @@ def future_busy(free_rooms: dict, entry_date: np.str_, amount_days: np.str_):
         if flag:
             final_rooms[room] = 0
     return final_rooms
+
+
+def profit(good_rooms: dict, opportunity: np.str_, arg_amount: int):
+    opportunity = int(opportunity)
+    rooms = []
+    list_for_max = []
+
+    if arg_amount == int(clt[4]):
+        for room in good_rooms:
+            rooms.append(room)
+            list_for_max.append(fund_dict[room][3])
+
+        list_for_max = np.array(list_for_max)
+        with_breakfast = list_for_max + food['breakfast'] * int(clt[6])
+        with_half_board = list_for_max + food['half_board'] * int(clt[6])
+        table_for_max = np.vstack([list_for_max, with_breakfast, with_half_board])
+
+        potential_max = table_for_max.max()
+        while opportunity < potential_max:
+            table_for_max[table_for_max == potential_max] = 0
+            potential_max = table_for_max.max()
+
+        if potential_max in list_for_max:
+            profit_room_index = np.where(list_for_max == potential_max)[0][0]
+        elif potential_max in with_breakfast:
+            profit_room_index = np.where(with_breakfast == potential_max)[0][0]
+        else:
+            profit_room_index = np.where(with_half_board == potential_max)[0][0]
+
+        profit_room = rooms[profit_room_index]
+        return profit_room, potential_max
+
+    else:
+        for room in good_rooms:
+            rooms.append(room)
+            list_for_max.append(fund_dict[room][4])
+
+        list_for_max = np.array(list_for_max)
+        with_breakfast = list_for_max + food['breakfast'] * int(clt[6])
+        with_half_board = list_for_max + food['half_board'] * int(clt[6])
+        table_for_max = np.vstack([list_for_max, with_breakfast, with_half_board])
+
+        potential_max = table_for_max.max()
+        while opportunity < potential_max:
+            table_for_max[table_for_max == potential_max] = 0
+            potential_max = table_for_max.max()
+
+        if potential_max in list_for_max:
+            profit_room_index = np.where(list_for_max == potential_max)[0][0]
+        elif potential_max in with_breakfast:
+            profit_room_index = np.where(with_breakfast == potential_max)[0][0]
+        else:
+            profit_room_index = np.where(with_half_board == potential_max)[0][0]
+
+        profit_room = rooms[profit_room_index]
+        return profit_room, potential_max
 
 
 type_room = {'one': 2900, 'two': 2300, 'middle_luxe': 3200, 'luxe': 4100}
@@ -78,8 +132,9 @@ with open('fund.txt', encoding= 'utf-8') as chrt:
 for key in fund_dict:
     coeff = fund_dict[key][2]
     tp_room = fund_dict[key][0]
-    fund_dict[key].append(type_room[tp_room]*coefficient[coeff])
-print(fund_dict)
+    fund_dict[key].append(int(type_room[tp_room] * coefficient[coeff]))
+    fund_dict[key].append(int(fund_dict[key][3] / 10 * 7))
+print(fund_dict, 'словарь fund_dict из файла fund')
 
 
 with open('booking.txt', encoding='utf-8') as clients:
@@ -105,50 +160,55 @@ days = []
 for i in range(31):
     days.append(np.str_(day_1 + np.timedelta64(i, 'D')))
 
-number_of_room = len(fund_dict)
-numbers = [np.str_(num) for num in range(1, number_of_room+1)]
+quantity_of_rooms = len(fund_dict)
+numbers = [np.str_(num) for num in range(1, quantity_of_rooms + 1)]
 
 busy = {day: dict.fromkeys(numbers, 0) for day in days}
-#print(busy)
-
+#print(busy, 'словарь состояний')
 
 for clt in matrix:
     date_entry = clt[5]
-    print(clt[1])
     free_numbers = busy[date_entry]
-    max_costs = clt[7]
-    amount = int(clt[4])
+    max_clt_cost = clt[7]       # сколько готов заплатить
+    amount = int(clt[4])        # сколько мест требуется челу
     print(free_numbers, 'до фильтров')
-    free_numbers = filter_cost(free_numbers, max_costs)
-    print(free_numbers, 'по цене и занятости на будущие даты')
+
+    flag_string = 'content'
     free_numbers = future_busy(free_numbers, date_entry, clt[6])
-    check_free_numbers = free_numbers.copy()
+    print(free_numbers, 'по занятости на будущие даты')     # первый фильтр применен
+    if len(free_numbers) == 0:      #!!!
+        flag_string = 'empty'
 
-    free_numbers = suitable_quantity_filter(free_numbers, amount)
-    if len(free_numbers) == 0:
-        if amount < 6:
-            for i in range(1, 7 - amount):
-                free_numbers = check_free_numbers
-                free_numbers = suitable_quantity_filter(free_numbers, amount + i)
-                print(amount + i)
-                if len(free_numbers) != 0:
-                    break
-                if amount + i == 6:
-                    print('Not free1')
-
+    while amount <= 6:
+        if len(suitable_quantity_filter(free_numbers, amount)) != 0:
+            free_numbers = suitable_quantity_filter(free_numbers, amount)
+            break
         else:
-            print('Not free')
+            amount += 1
 
-    if len(free_numbers) != 0:
-        max_cost = max(fund_dict[key][3] for key in free_numbers.keys())
-        for room in free_numbers:
-            if fund_dict[room][1] != amount:
-                max_cost = max_cost * 0.7
-                break
+    if amount == 7:
+        free_numbers = {}
+        flag_string = 'empty'
+    print(free_numbers, 'по нужному количеству человек')        # второй фильтр применен
 
-        print(max_cost)
-        print(free_numbers)
-    print(free_numbers)
+    free_numbers = filter_cost(free_numbers, max_clt_cost, amount)
+    print(free_numbers, 'по цене')
+    if len(free_numbers) == 0:      #!!! x2
+        flag_string = 'empty'
+
+    if flag_string == 'content':
+        # подсчитать профит (сделано, реализовал функцию profit)
+        clt_profit = profit(free_numbers, max_clt_cost, amount)[1]
+        room_for_clt = profit(free_numbers, max_clt_cost, amount)[0]
+        print(room_for_clt, 'комната для клиента')
+        print(clt_profit, 'выручка')
+    else:
+        pass
+        # подсчитать убыток
+
+    # после того, как все посчитали, надо изменить словарь состояний busy и можно в следующую итерацию
+    # в начале цикла тоже нужно будет реализовать, чтоб "с утра" все понаехавшие съезжали
+
     break
 
 
